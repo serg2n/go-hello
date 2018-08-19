b10dc75ec9b04a6ee4e280340e6e04bd0fb66084
Serg <box10@stim.by>
2018-08-19 19:21:41 +0300

    1

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Привет Hello from Go")
+		fmt.Fprintf(w, "Привет GitHub Hello from Go")
 	})
 	http.ListenAndServe(":3000", nil)
 }
